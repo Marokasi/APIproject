5ff62ec28df6c3c5ea74f21d1495bb7d693ca4b5
Max Plokhuta <max.plokhuta@exemplarsystems.com>
2019-10-30 10:12:36 -0400

    Removed https redirection

--- src/APIproject/Startup.cs
+++ src/APIproject/Startup.cs
@@ -36,7 +36,7 @@ namespace APIproject
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
